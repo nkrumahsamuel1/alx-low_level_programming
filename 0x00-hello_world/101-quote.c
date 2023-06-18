ba7beaf7e82f30a0ec1c32475d63bf14e128d136
nkrumahsamuel1 <nkrumahsamuel410@yahoo.com>
2023-06-18 15:50:56 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - a program to print a quote into error mode
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
