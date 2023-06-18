0b40e6c79d4d76b81005450c1019c333478605b3
nkrumahsamuel1 <nkrumahsamuel410@yahoo.com>
2023-06-18 15:18:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - a program to print all single digital numbers base 10.
+*
+* Return:0 (Success)
+*/
+
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+
+printf("\n");
+
+return (0);
+}
