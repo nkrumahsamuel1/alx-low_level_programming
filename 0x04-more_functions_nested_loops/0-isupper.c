d979dfbbf0ec235458576f30063fa2bd45852129
nkrumahsamuel1 <nkrumahsamuel410@yahoo.com>
2023-06-23 14:03:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - check id a letter us upper
+ * _isupper - check if a letter us upper
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 fot else
  */
